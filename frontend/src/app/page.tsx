53d2b57654a19d4f12e85d5c5e4c1c26f0e68d0f
Subham Mondal <subhamoffi@gmail.com>
2025-11-24 19:59:18 +0530

    Differentiate the message implemented room join input field

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,6 +3,11 @@ import { useEffect, useRef, useState } from "react";
 import { DefaultEventsMap } from "socket.io";
 import { io, Socket } from "socket.io-client";
 
+type MessageType = {
+  userId: string;
+  message: string;
+};
+
 export default function Home() {
   // const [socket, setSocket] = useState<Socket<DefaultEventsMap> | null>(null);
 
@@ -31,14 +36,28 @@ export default function Home() {
   //   console.log(socket);
   // }, [socket]);
   const [inputMessage, setInputMessage] = useState("");
-  const [messages, setMessages] = useState<string[]>(["hello", "hi"]);
+  const [messages, setMessages] = useState<MessageType[]>([
+    {
+      userId: "xyz",
+      message: "hello there",
+    },
+  ]);
+  const [inputRoomId, setInputRoomId] = useState("");
+  const [roomId, setRoomId] = useState("");
+  const [currentUserId, setCurrentUserId] = useState("");
 
   const handleOnchange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputMessage(e.target.value);
   };
-
+  const handleRoomId = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputRoomId(e.target.value);
+  };
+  const handleJoinRoom = () => {
+    setRoomId(inputRoomId);
+  };
   const handleSendMessage = () => {
     console.log(inputMessage);
+    if (!inputMessage) return;
     socketRef.current?.emit("user-message", inputMessage);
     setInputMessage("");
   };
@@ -51,6 +70,7 @@ export default function Home() {
     console.log(s);
     s.on("connect", () => {
       console.log("connected to backend", s.id);
+      setCurrentUserId(s.id || "");
     });
 
     s.on("message", (msg) => {
@@ -59,12 +79,37 @@ export default function Home() {
     });
   }, []);
   return (
-    <div className="max-w-lg mx-auto">
-      Chat App
+    <div className="max-w-md mx-auto">
+      <h1 className="font-bold  mx-auto text-center italic text-2xl">
+        Chat App
+      </h1>
+      <div>
+        {roomId ? (
+          <div className="px-5 py-1 bg-neutral-100  w-fit rounded-md shadow-sm inset-shadow-sm mb-3">
+            {roomId}
+          </div>
+        ) : (
+          <div>
+            <input
+              type="text"
+              className="outline-none mb-3 bg-neutral-100 py-1 mr-2 rounded-md px-2 border border-neutral-200 placeholder:text-neutral-400 "
+              placeholder="Enter RoomId"
+              value={inputRoomId}
+              onChange={handleRoomId}
+            />
+            <button
+              className="bg-neutral-900 px-3 py-1 text-white rounded-md shadow-[0px_2px_4px_1px_rgba(255,255,255,0.5)_inset,0px_-2px_4px_1px_rgba(255,255,255,0.5)_inset,2px_0px_4px_1px_rgba(255,255,255,0.5)_inset]"
+              onClick={handleJoinRoom}
+            >
+              Join
+            </button>
+          </div>
+        )}
+      </div>
       <div className="mb-10">
         <input
           type="text"
-          className="outline-none bg-neutral-100 py-1 mr-2 rounded-md px-2 border border-neutral-200 placeholder:text-neutral-400 shadow-[0px_2px_4px_1px_rgba(0,0,0,0.1)_inset,0px_-2px_4px_1px_rgba(0,0,0,0.1)_inset]"
+          className="outline-none bg-neutral-100 py-1 mr-2 rounded-md px-2 border border-neutral-200 placeholder:text-neutral-400 "
           placeholder="Enter message..."
           value={inputMessage}
           onChange={handleOnchange}
@@ -77,12 +122,20 @@ export default function Home() {
           Send
         </button>
       </div>
-      <div className="overflow-y-scroll flex flex-col gap-2 bg-cyan-300 h-70 w-100">
-        {messages?.map((text, idx) => {
-          return (
-            <p className="py-1 bg-neutral-500" key={idx}>
-              {text}
-            </p>
+      <div className="overflow-y-scroll flex flex-col gap-2 bg-neutral-100 rounded-md shadow  h-70 w-100 no-scrollbar-arrows p-2">
+        {messages?.map(({ userId, message }, idx) => {
+          return userId == currentUserId ? (
+            <div key={idx} className="flex justify-end">
+              <p className="py-1 bg-green-300 w-fit px-1 rounded-l-lg rounded-tr-2xl relative">
+                {message}
+              </p>
+            </div>
+          ) : (
+            <div key={idx} className="">
+              <p className="py-1 bg-cyan-300 w-fit px-1 rounded-r-lg rounded-tl-2xl">
+                {message}
+              </p>
+            </div>
           );
         })}
       </div>
